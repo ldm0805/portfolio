ab2b27d4dfb2eb552d58ae237b345f57099e3674
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2023-04-17 15:08:27 +0200

    Modal fatta

--- src/pages/ContattiPage.vue
+++ src/pages/ContattiPage.vue
@@ -17,16 +17,13 @@ export default {
   },
   methods: {
     validateEmail(value) {
-      // if the field is empty
       if (!value) {
         return "La mail è richiesta";
       }
-      // if the field is not a valid email
       const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
       if (!regex.test(value)) {
         return "Devi inserire una mail valida";
       }
-      // All is good
       return true;
     },
     validateRequest(value) {
@@ -48,7 +45,7 @@ export default {
         email: this.email,
         message: this.message,
       };
-      if(this.name && this.email && this.message != ''){
+      if(this.name && this.email && this.message != '' ){
 
         const serviceID = "service_87wg2n8";
         const templateID = "template_1fzse0z";
@@ -94,53 +91,73 @@ export default {
         </a>
     </div>
 
-      <div class="col-md-6 border-left py-3">
-          <h1>Contattami!</h1>
-          <Form id="my-form">
-          <div class="form-group">
-            <h5 for="name">Nome</h5>
-            <Field
-              type="text"
-              class="form-control"
-              name="name"
-              id="name"
-              placeholder="Inserisci il tuo nome"
-              v-model="name"
-              :rules="validateRequest"
-            />
-            <ErrorMessage class="text-danger" name="name" />
+    <button type="button" class="portfolio-link" data-toggle="modal" data-target="#exampleModal">
+        Mandami un messaggio
+    </button>
 
-          </div>
-          <div class="form-group">
-            <h5 for="email">Email</h5>
-            <Field
-              type="email"
-              class="form-control"
-              name="email"
-              id="email"
-              placeholder="Inserisci l'email"
-              v-model="email"
-              :rules="validateEmail"
-            />
-            <ErrorMessage class="text-danger" name="email" />
-          </div>
-          <div class="form-group">
-            <h5 for="message">Messaggio</h5>
-            <Field 
-            as="textarea"
-            class="form-control" 
-            name="message"
-            id="message" 
-            rows="3"  
-            v-model="message"
-            :rules="[validateRequest, validateLength5]"
-            placeholder="Messaggio"
-            />
-            <ErrorMessage class="text-danger" name="message" />
-          </div>
-          <button class="btn btn-primary" @click="sendMail()">Submit</button>
-          </Form>
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Contattami!</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
       </div>
+      <div class="modal-body">
+        <div class="col-md-6 border-left py-3">
+          <Form id="my-form" method="POST">
+            <div class="form-group">
+              <h5 for="name">Nome</h5>
+              <Field
+                type="text"
+                class="form-control"
+                name="name"
+                id="name"
+                placeholder="Inserisci il tuo nome"
+                v-model="name"
+                :rules="validateRequest"
+              />
+              <ErrorMessage class="text-danger" name="name" />
+            </div>
+            <div class="form-group">
+              <h5 for="email">Email</h5>
+              <Field
+                type="email"
+                class="form-control"
+                name="email"
+                id="email"
+                placeholder="Inserisci l'email"
+                pattern=".+@globex\.com"
+                v-model="email"
+                :rules="validateEmail"
+              />
+              <ErrorMessage class="text-danger" name="email" />
+            </div>
+            <div class="form-group">
+              <h5 for="message">Messaggio</h5>
+              <Field 
+                as="textarea"
+                class="form-control" 
+                name="message"
+                id="message" 
+                rows="3"  
+                v-model="message"
+                :rules="[validateRequest, validateLength5]"
+                placeholder="Messaggio"
+              />
+              <ErrorMessage class="text-danger" name="message" />
+            </div>
+          </Form>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button class="portfolio-link" @click="sendMail()"  data-dismiss="modal">Invia dati</button>
+      </div>
+    </div>
+  </div>
+</div>
   </div>
 
 
@@ -158,7 +175,31 @@ div{
         }
     }
 }
+button{
+  all: unset;
+}
+.portfolio-link{
+    display: inline-block;
+    margin-top: 1em;
+    padding: .5em 1em;
+    text-decoration: none;
+    border: 2px solid black;
+    color: $font_color;
+    &:hover{
+        scale: 1.1;
+        box-shadow: black 3px 3px;
+        
+    }
     
+}
+
+.modal{
+  --bs-modal-bg: #393646 !important;
+
+}
+  div.modal-contet{
+      background-color: #393646 !important;
+    }
 </style>
 
     
