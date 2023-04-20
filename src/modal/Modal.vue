53621529095827211719405ad8bfa19fbd05ed3c
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2023-04-20 06:13:15 +0200

    Modale inserita in una componente

---
+++ src/modal/Modal.vue
@@ -0,0 +1,149 @@
+<script>
+import { Form, Field, ErrorMessage } from "vee-validate";
+
+export default {
+  components: {
+    Form,
+    Field,
+    ErrorMessage,
+  },
+  data() {
+    return {
+      name: "",
+      email: "",
+      message: "",
+      errors: {},
+    };
+  },
+  methods: {
+    validateEmail(value) {
+      if (!value) {
+        return "La mail è richiesta";
+      }
+      const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+      if (!regex.test(value)) {
+        return "Devi inserire una mail valida";
+      }
+      return true;
+    },
+    validateRequest(value) {
+      // if the field is empty
+      if (!value) {
+        return "Il campo è richiesto";
+      }
+      return true;
+    },
+    validateLength5(value) {
+      if (value.length < 5) {
+        return "Il campo deve contenere minimo 5 caratteri";
+      }
+      return true;
+    },
+    sendMail() {
+      let params = {
+        name: this.name,
+        email: this.email,
+        message: this.message,
+      };
+      if (this.name && this.email && this.message != "") {
+        const serviceID = "service_87wg2n8";
+        const templateID = "template_1fzse0z";
+
+        emailjs
+          .send(serviceID, templateID, params)
+          .then((res) => {
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("message").value = "";
+            console.log(res);
+            alert("Your message sent successfully!!");
+          })
+          .catch((err) => console.log(err));
+      }
+    },
+  },
+};
+</script>
+<template lang="">
+  <!-- Modal -->
+  <div
+    class="modal fade"
+    id="exampleModal"
+    tabindex="-1"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Contattami!</h5>
+          <button
+            type="button"
+            class="close"
+            data-dismiss="modal"
+            aria-label="Close"
+          >
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <div class="col-md-6 border-left py-3">
+            <Form id="my-form" method="POST">
+              <div class="form-group">
+                <h5 for="name">Nome</h5>
+                <Field
+                  type="text"
+                  class="form-control"
+                  name="name"
+                  id="name"
+                  placeholder="Inserisci il tuo nome"
+                  v-model="name"
+                  :rules="validateRequest"
+                />
+                <ErrorMessage class="text-danger" name="name" />
+              </div>
+              <div class="form-group">
+                <h5 for="email">Email</h5>
+                <Field
+                  type="email"
+                  class="form-control"
+                  name="email"
+                  id="email"
+                  placeholder="Inserisci l'email"
+                  pattern=".+@globex\.com"
+                  v-model="email"
+                  :rules="validateEmail"
+                />
+                <ErrorMessage class="text-danger" name="email" />
+              </div>
+              <div class="form-group">
+                <h5 for="message">Messaggio</h5>
+                <Field
+                  as="textarea"
+                  class="form-control"
+                  name="message"
+                  id="message"
+                  rows="3"
+                  v-model="message"
+                  :rules="[validateRequest, validateLength5]"
+                  placeholder="Messaggio"
+                />
+                <ErrorMessage class="text-danger" name="message" />
+              </div>
+            </Form>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button
+            class="portfolio-link"
+            @click="sendMail()"
+            data-dismiss="modal"
+          >
+            Invia dati
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style lang=""></style>
--- src/pages/ContattiPage.vue
+++ src/pages/ContattiPage.vue
@@ -1,205 +1,144 @@
 <script>
-import { Form, Field, ErrorMessage } from "vee-validate";
-
+import Modal from "../modal/modal.vue";
 export default {
-    components: {
-    Form,
-    Field,
-    ErrorMessage,
+  components: {
+    Modal,
   },
-  data() {
-    return {
-      name: "",
-      email: "",
-      message: "",
-      errors: {},
-    };
-  },
-  methods: {
-    validateEmail(value) {
-      if (!value) {
-        return "La mail è richiesta";
-      }
-      const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
-      if (!regex.test(value)) {
-        return "Devi inserire una mail valida";
-      }
-      return true;
-    },
-    validateRequest(value) {
-      // if the field is empty
-      if (!value) {
-        return "Il campo è richiesto";
-      }
-      return true;
-    },
-    validateLength5(value) {
-      if (value.length < 5) {
-        return "Il campo deve contenere minimo 5 caratteri";
-      }
-      return true;
-    },
-    sendMail() {
-      let params = {
-        name: this.name,
-        email: this.email,
-        message: this.message,
-      };
-      if(this.name && this.email && this.message != '' ){
+};
 
-        const serviceID = "service_87wg2n8";
-        const templateID = "template_1fzse0z";
-        
-      emailjs.send(serviceID, templateID, params)
-        .then(res => {
-          document.getElementById("name").value = "";
-          document.getElementById("email").value = "";
-          document.getElementById("message").value = "";
-          console.log(res);
-          alert("Your message sent successfully!!")
-          
-        })
-        .catch(err => console.log(err));
-      }
-    }
-  }
-}
+// import { Form, Field, ErrorMessage } from "vee-validate";
 
+// export default {
+//   components: {
+//     Form,
+//     Field,
+//     ErrorMessage,
+//   },
+//   data() {
+//     return {
+//       name: "",
+//       email: "",
+//       message: "",
+//       errors: {},
+//     };
+//   },
+//   methods: {
+//     validateEmail(value) {
+//       if (!value) {
+//         return "La mail è richiesta";
+//       }
+//       const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+//       if (!regex.test(value)) {
+//         return "Devi inserire una mail valida";
+//       }
+//       return true;
+//     },
+//     validateRequest(value) {
+//       // if the field is empty
+//       if (!value) {
+//         return "Il campo è richiesto";
+//       }
+//       return true;
+//     },
+//     validateLength5(value) {
+//       if (value.length < 5) {
+//         return "Il campo deve contenere minimo 5 caratteri";
+//       }
+//       return true;
+//     },
+//     sendMail() {
+//       let params = {
+//         name: this.name,
+//         email: this.email,
+//         message: this.message,
+//       };
+//       if (this.name && this.email && this.message != "") {
+//         const serviceID = "service_87wg2n8";
+//         const templateID = "template_1fzse0z";
+
+//         emailjs
+//           .send(serviceID, templateID, params)
+//           .then((res) => {
+//             document.getElementById("name").value = "";
+//             document.getElementById("email").value = "";
+//             document.getElementById("message").value = "";
+//             console.log(res);
+//             alert("Your message sent successfully!!");
+//           })
+//           .catch((err) => console.log(err));
+//       }
+//     },
+//   },
+// };
 </script>
 <template lang="">
-  <div class="cont d-flex justify-content-center flex-column align-items-center">
-    
-
+  <div
+    class="cont d-flex justify-content-center flex-column align-items-center"
+  >
     <div class="d-flex flex-column gap-3">
-        <div>
-            Contatti 
-        </div>
-        <a href="https://github.com/ldm0805"> 
-            <i class="fa-brands fa-square-github fa-xl"></i> GitHub
-        </a>
-        <a href="mailto:leonardodimaggio94@gmail.com">
-            <i class="fa-solid fa-envelope fa-xl"></i> Email
-        </a>
-        <a href="https://www.linkedin.com/in/leonardo-di-maggio-4ba403268/">
-            <i class="fa-brands fa-linkedin fa-xl"></i> Linkedin
-        </a>
-        <a href="../../public/curriculum/Leonardo-Di-Maggio-cv.pdf" download>
-            <i class="fa-solid fa-user fa-xl"></i> Curriculum
-        </a>
-        <a href="#">
-            <i class="fa-solid fa-mobile fa-xl"></i> Num Telefono
-        </a>
+      <div>Contatti</div>
+      <a href="https://github.com/ldm0805">
+        <i class="fa-brands fa-square-github fa-xl"></i> GitHub
+      </a>
+      <a href="mailto:leonardodimaggio94@gmail.com">
+        <i class="fa-solid fa-envelope fa-xl"></i> Email
+      </a>
+      <a href="https://www.linkedin.com/in/leonardo-di-maggio-4ba403268/">
+        <i class="fa-brands fa-linkedin fa-xl"></i> Linkedin
+      </a>
+      <a href="../../public/curriculum/Leonardo-Di-Maggio-cv.pdf" download>
+        <i class="fa-solid fa-user fa-xl"></i> Curriculum
+      </a>
+      <a href="tel:+393384577144">
+        <i class="fa-solid fa-mobile fa-xl"></i> Num Telefono
+        <!-- +393384577144 -->
+      </a>
     </div>
 
-    <button type="button" class="portfolio-link" data-toggle="modal" data-target="#exampleModal">
-        Mandami un messaggio
+    <button
+      type="button"
+      class="portfolio-link"
+      data-toggle="modal"
+      data-target="#exampleModal"
+    >
+      Mandami un messaggio
     </button>
-
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Contattami!</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <div class="col-md-6 border-left py-3">
-          <Form id="my-form" method="POST">
-            <div class="form-group">
-              <h5 for="name">Nome</h5>
-              <Field
-                type="text"
-                class="form-control"
-                name="name"
-                id="name"
-                placeholder="Inserisci il tuo nome"
-                v-model="name"
-                :rules="validateRequest"
-              />
-              <ErrorMessage class="text-danger" name="name" />
-            </div>
-            <div class="form-group">
-              <h5 for="email">Email</h5>
-              <Field
-                type="email"
-                class="form-control"
-                name="email"
-                id="email"
-                placeholder="Inserisci l'email"
-                pattern=".+@globex\.com"
-                v-model="email"
-                :rules="validateEmail"
-              />
-              <ErrorMessage class="text-danger" name="email" />
-            </div>
-            <div class="form-group">
-              <h5 for="message">Messaggio</h5>
-              <Field 
-                as="textarea"
-                class="form-control" 
-                name="message"
-                id="message" 
-                rows="3"  
-                v-model="message"
-                :rules="[validateRequest, validateLength5]"
-                placeholder="Messaggio"
-              />
-              <ErrorMessage class="text-danger" name="message" />
-            </div>
-          </Form>
-        </div>
-      </div>
-      <div class="modal-footer">
-        <button class="portfolio-link" @click="sendMail()"  data-dismiss="modal">Invia dati</button>
-      </div>
-    </div>
+    <Modal />
   </div>
-</div>
-  </div>
-
-
 </template>
 <style lang="scss" scoped>
 @use "../styles/partials/variables" as *;
-div{
+div {
+  color: $font_color;
+  a {
+    text-decoration: none;
     color: $font_color;
-    a{
-        text-decoration: none;
-        color: $font_color;
-        transition: 300ms;
-        &:hover{
-            color: rgb(0, 0, 87);
-        }
+    transition: 300ms;
+    &:hover {
+      color: rgb(0, 0, 87);
     }
+  }
 }
-button{
+button {
   all: unset;
 }
-.portfolio-link{
-    display: inline-block;
-    margin-top: 1em;
-    padding: .5em 1em;
-    text-decoration: none;
-    border: 2px solid black;
-    color: $font_color;
-    &:hover{
-        scale: 1.1;
-        box-shadow: black 3px 3px;
-        
-    }
-    
+.portfolio-link {
+  display: inline-block;
+  margin-top: 1em;
+  padding: 0.5em 1em;
+  text-decoration: none;
+  border: 2px solid black;
+  color: $font_color;
+  &:hover {
+    scale: 1.1;
+    box-shadow: black 3px 3px;
+  }
 }
 
-.modal{
+.modal {
   --bs-modal-bg: #393646 !important;
-
 }
-  div.modal-contet{
-      background-color: #393646 !important;
-    }
+div.modal-contet {
+  background-color: #393646 !important;
+}
 </style>
-
-    
